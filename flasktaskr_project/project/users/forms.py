34656d0ece99870d333fdc6354990d1658d9b57a
Mike Perham <mkperham@gmail.com>
2015-01-14 21:58:59 -0600

    moved into project blueprints

--- flasktaskr_project/project/users/forms.py
+++ flasktaskr_project/project/users/forms.py
@@ -1,28 +1,9 @@
-# forms.py
+# /project/users/forms.py
 
 from flask_wtf import Form
 from wtforms import TextField, DateField, IntegerField, \
     SelectField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
-
-class AddTaskForm(Form):
-    task_id = IntegerField('Priority')
-    name = TextField('Task Name', validators=[DataRequired()])
-    due_date = DateField(
-                         'Date Due (mm/dd/yyyy)',
-                          validators=[DataRequired()], format='%m/%d/%Y'
-                        )
-    priority = SelectField(
-                           'Priority',
-                           validators = [DataRequired()],
-                           choices=[
-                           ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),
-                           ('5', '5'),
-                           ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'),
-                           ('10', '10')
-                           ]
-                           )
-    status = IntegerField('Status')
 
 class RegisterForm(Form):
     name = TextField(
@@ -38,7 +19,8 @@ class RegisterForm(Form):
     password = PasswordField(
             'Password',
             validators=[DataRequired(),
-            Length(min=6, max=40)])
+            Length(min=6, max=40)]
+        )
     confirm = PasswordField(
             'Repeat Password',
             validators=[DataRequired(),
